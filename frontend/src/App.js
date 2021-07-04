04889c616eb184b3313aa225d033f48b2d3ea1ae
erdldncr <erdldncr@gmail.com>
2021-07-04 22:47:59 +0100

    product details page addded

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,7 +12,7 @@ function App() {
       <main className="py-3">
         <Container>
           <Route path="/" exact component={HomeScreen} />
-          <Route path="/prodduct/:id" component={ProductScreen} />
+          <Route path="/product/:id" component={ProductScreen} />
         </Container>
       </main>
 
--- frontend/src/Screens/ProductScreen.js
+++ frontend/src/Screens/ProductScreen.js
@@ -1,7 +1,70 @@
-import React from "react";
-
-const ProductScreen = () => {
-  return <div>Product</div>;
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import products from "../products";
+import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
+import Rating from "../components/Rating";
+const ProductScreen = (props) => {
+  const { id } = useParams();
+  const [product, setProduct] = useState({});
+  useEffect(() => {
+    setProduct(products.find((product) => product._id === id));
+  }, []);
+  return (
+    <>
+      <Link className="btn btn-light my-3">Go Back</Link>
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+              />
+            </ListGroup.Item>
+            <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+            <ListGroup.Item>Description: ${product.description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    {product.countInStock > 0 ? "In Stock" : "Out Of Stock"}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button
+                  disabled={product.countInStock === 0}
+                  className="btn-block"
+                  type="button"
+                >
+                  Add to Cart
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default ProductScreen;
