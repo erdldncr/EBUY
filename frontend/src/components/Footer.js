5dc2ff2c4506cc3c84d0da1fda524d831866384b
erdldncr <erdldncr@gmail.com>
2021-07-04 17:20:48 +0100

    headeer and footer compoents added and designed

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+const Footer = () => {
+  return (
+    <footer className="">
+      <Container>
+        <Row>
+          <Col className="text-center py-3">Copyright &copy; Ebuy</Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
