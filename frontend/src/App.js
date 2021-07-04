c883110dbe815581b71e18c3238846ef4b521363
erdldncr <erdldncr@gmail.com>
2021-07-04 21:56:48 +0100

    react-router addded

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,23 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HomeScreen from "./Screens/HomeScreen";
+import ProductScreen from "./Screens/ProductScreen";
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Route path="/" exact component={HomeScreen} />
+          <Route path="/prodduct/:id" component={ProductScreen} />
         </Container>
       </main>
 
       <Footer />
-    </>
+    </Router>
   );
 }
 
