988a99e938ec4e15b2d7df849263f383031b86c5
erdldncr <erdldncr@gmail.com>
2021-09-01 12:18:12 +0100

    checkout steps added

--- frontend/src/Screens/ShippingScreen.js
+++ frontend/src/Screens/ShippingScreen.js
@@ -2,14 +2,15 @@ import React, { useState } from "react";
 import { Form, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import FormContainer from "../components/FormContainer";
+import CheckoutSteps from "../components/CheckoutSteps";
 import { saveShippingAddress } from "../actions/cartActions";
 const ShippingScreen = ({ history }) => {
   const cart = useSelector((state) => state.cart);
   const { shippingAddress } = cart;
-  const [address, setAddress] = useState(shippingAddress.address);
-  const [city, setCity] = useState(shippingAddress.city);
-  const [postalCode, setPostalCode] = useState(shippingAddress.postalCode);
-  const [country, setCountry] = useState(shippingAddress.country);
+  const [address, setAddress] = useState(shippingAddress?.address);
+  const [city, setCity] = useState(shippingAddress?.city);
+  const [postalCode, setPostalCode] = useState(shippingAddress?.postalCode);
+  const [country, setCountry] = useState(shippingAddress?.country);
   const dispatch = useDispatch();
 
   const submitHandler = (e) => {
@@ -19,6 +20,7 @@ const ShippingScreen = ({ history }) => {
   };
   return (
     <FormContainer>
+      <CheckoutSteps step1 step2 />
       <h1>ShippingScreen</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group controlId="address">
