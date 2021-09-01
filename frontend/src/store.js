2e7f397b0a390bdd598911234bd57ebebea0c96c
erdldncr <erdldncr@gmail.com>
2021-09-01 14:55:23 +0100

    place order componenet added

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -30,11 +30,15 @@ const userInfoFromStorage = localStorage.getItem("userInfo")
 const shippingAddressFromStorage = localStorage.getItem("shippingAddress")
   ? JSON.parse(localStorage.getItem("shippingAddress"))
   : {};
+const paymentMethod = localStorage.getItem("paymentMethod")
+  ? JSON.parse(localStorage.getItem("paymentMethod"))
+  : {};
 
 const initialState = {
   cart: {
     cartItems: cartItemsFromStorage,
     shippingAddress: shippingAddressFromStorage,
+    paymentMethod,
   },
   userLogin: { userInfo: userInfoFromStorage },
 };
